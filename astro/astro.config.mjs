fec582b8eb78ca2646d914f5f051a2f717c16778
fauzaanu <fauzaangasim@gmail.com>
2024-06-19 00:32:39 +0500

    feat: another domain, another day

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -26,7 +26,7 @@ const whenExternalScripts = (items = []) =>
 
 export default defineConfig({
   output: 'static',
-  site: 'https://fauzaanu.github.io/quizpal/',
+  site: 'https://quizpalbot.com/',
 
   integrations: [
     tailwind({
