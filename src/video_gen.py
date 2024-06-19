90d3d129eda914561cabcf0728cc259301759c64
fauzaanu <fauzaangasim@gmail.com>
2024-06-19 06:04:54 +0500

    feat: started looking into the process for video generation for user generated content.

---
+++ src/video_gen.py
@@ -0,0 +1,10 @@
+"""This starts the work on quizpal video generations.
+
+As twitter is running now this can be the content we are looking for. but we also need this for tiktok.
+Then again twitter does work the same way as tiktok and elon has signaled that such a feed may appear very soon.
+So we will use the tiktok format for video generations. This wouldnt be bad for telegram as well as we can post them as stories.
+Looks like tiktok has set a trend.
+
+Furthermore, telegram beta version has added the ability to add links to stories. this would indeed be very cool if implemented in time.
+"""
+
