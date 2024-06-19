82e73683277d3afdf479b21b9839d92e157869a9
fauzaanu <fauzaangasim@gmail.com>
2024-06-19 20:01:29 +0500

    feat: update intro message to keep things short and simpler

--- src/constants.py
+++ src/constants.py
@@ -6,28 +6,16 @@ TELEGRAM_QUIZ_OPTION_LIMIT = 100
 TELEGRAM_QUIZ_EXPLANATION_LIMIT = 200
 
 INTRO_MESSAGE = (
-        escape_dot("🌟 *Welcome to Quizpal* 🌟\n\n") +
-        escape_dot("By using this bot you agree to our terms and conditions on: quizpalbot.com/terms\n\n") +
-        escape_dot("📚 Send any topic to generate questions!\n") +
-        escape_dot("💫 Each question costs 1 Quizpal Star.\n") +
-        escape_dot("💫 You start with 25 Quizpal Stars.\n\n") +
+    escape_dot("Hi, I'm QuizpalBot 👋, ready to send you Telegram quizzes"
+               " on any topic you choose. What would you like to learn about?")
+)
 
-        escape_dot("🌟 *Earn Stars:*\n") +
-        escape_dot("🎉 Each correct answer refunds the cost.\n") +
-        escape_dot("🌟 Get a win streak to earn more stars.\n") +
-        escape_dot("❌ Incorrect answers resets the win streak.\n\n") +
-
+COMMANDS = (
         escape_dot("⚙️ *Commands:*\n") +
         escape_dot("🔹 /topics - Explore available topics.\n") +
         escape_dot("🔹 /balance - Check your star balance.\n") +
         escape_dot("🔹 /help - Watch the tutorial video.\n") +
-        escape_dot("🔹 /topup - Add stars to your balance.\n\n") +
-
-        escape_dot("🔍 *Examples of topics:*\n") +
-        escape_dot("🔹 `Few Shot Learning with Prototypical Networks`\n") +
-        escape_dot("🔹 `Cognitive Load Theory in Instructional Design`\n") +
-        escape_dot("🔹 `Clinical Pharmacokinetics and Pharmacodynamics`\n\n") +
-        escape_dot("📚 *Start by sending a topic!* 📚\n")
+        escape_dot("🔹 /topup - Add stars to your balance.\n\n")
 )
 
 CELEBRATION_EFFECT_ID = 5046509860389126442
